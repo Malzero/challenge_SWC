204aebfa5bf80eb30f62caa3b92395f50d6ef6a3
Mauricio Rivera <kyonjedi@gmail.com>
2024-09-28 03:49:57 -0300

    added axios

--- FrontEnd/challenge_SWC/src/main.js
+++ FrontEnd/challenge_SWC/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import PortalVue from 'portal-vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 
 //import './assets/main.css'
@@ -11,6 +13,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(PortalVue)
+Vue.use(VueAxios, axios)
 
 new Vue({
   render: (h) => h(App)
